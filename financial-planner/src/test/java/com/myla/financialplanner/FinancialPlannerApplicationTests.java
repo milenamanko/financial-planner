6f353032a68dbea4d5ec3feb6cfea0454d51ae2f
milenamanko <milenamankowska@gmail.com>
2020-10-07 12:02:22 +0200

    initial commit

---
+++ financial-planner/src/test/java/com/myla/financialplanner/FinancialPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myla.financialplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
